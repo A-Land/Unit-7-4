e690b772dfa03b9cb947e7db21109f67f271ec94
Adam Landriault <>
2021-05-10 21:21:25 +0000

    fixed errors

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const day = 'Monday'
 const age = 37
-if ((day === 'Tuesday' || day === 'Thursday')||(age > 12 && age < 21 )) {
+if ((day === 'Tuesday' || day === 'Thursday') || (age > 12 && age < 21)) {
   console.log("You're eligible for student pricing!")
 } else {
   console.log('You must pay regular price.')
