f6c9bd399cc15fe4c4ee2438c2920c4d97e30f3b
AdamLandriault <>
2021-05-10 21:09:02 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+var day = 'Monday'
+var age = 15
+if ((day == 'Tuesday' || day == 'Thursday') || (age > 12 && age < 21 )) {
+console.log ("You're eligible for student pricing!")
+} else {
+console.log ('You must pay regular price.')
+}
